2d8116b950d2e65948df70f67a3d19f82b9f2769
base1551 <baseball.1.kyoto@gmail.com>
2022-01-15 23:18:11 +0900

    デザイン微修正

--- components/Layout.js
+++ components/Layout.js
@@ -13,23 +13,23 @@ export default function Layout({ children, title = "HP by Nextjs" }) {
       </Head>
       <header>
         {/* 全体のnavバー */}
-        <nav className="bg-gray-800 w-screen">
+        <nav className="bg-blue-300 w-screen">
           <div className="flex items-center pl-8 h-14">
-            <div className="flex space-x-4">
+            <div className="text-lg flex space-x-4">
               {/* reactのデフォルト Linkコンポーネント hrefのところに遷移したいパスを指定できる*/}
               {/* nextjsではpageディレクトリにあるパス名がデフォルトでルートに対応する*/}
               <Link href="/">
-                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rouded">
+                <a className="text-black-300 hover:bg-blue-300 px-3 py-2 rouded">
                   Home
                 </a>
               </Link>
               <Link href="blog-page">
-                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rouded">
+                <a className="text-black-300 hover:bg-blue-300 px-3 py-2 rouded">
                   Blog
                 </a>
               </Link>
               <Link href="book-page">
-                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rouded">
+                <a className="text-black-300 hover:bg-blue-300 px-3 py-2 rouded">
                   Books
                 </a>
               </Link>
