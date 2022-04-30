3c77919d871af08174073625379ba3f376a9b7b0
base1551 <baseball.1.kyoto@gmail.com>
2022-04-30 14:08:54 +0900

    デザイン修正

--- components/TableOfContents.js
+++ components/TableOfContents.js
@@ -1,10 +1,10 @@
 export const TableOfContents = ({ toc }) => {
     return (
         <div>
-            <p className="TableOfContentsHead">目次</p>
+            <p className="p-3 m-10 bg-gray-100 border border-black border-dashed TableOfContentsHead">目次</p>
             <ul>
                 {toc.map(data => (
-                    <li key={data.id}>
+                    <li className="p-2 list-decimal list-inside" key={data.id}>
                         <a href={`#${data.id}`}>
                             {data.text}
                         </a>
