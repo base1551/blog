f278521d46e4815c9984e5b1937fdfc1d8b9cafe
base1551 <baseball.1.kyoto@gmail.com>
2022-05-18 23:46:25 +0900

    不要処理削除

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Link from "next/link";
-import Image from "next/image";
 import Profile from "./Profile";
 
 //title = ブラウザのタイトル
@@ -15,33 +14,11 @@ export default function Layout({ children, title = "HP by Nextjs" }) {
       <header className="relative flex items-center justify-between py-4 lg:px-20 px-4 border-b">
         <div className="flex items-center">
           <Link className="active" href="/">
-            <a href="/home" className="text-2xl font-bold">
+            <a className="text-2xl font-bold">
               OkaBlog
             </a>
           </Link>
         </div>
-        {/* <button className="md:hidden block">
-          <svg
-            fill="currentColor"
-            height="20"
-            viewBox="0 0 24 24"
-            width="20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M4 22h-4v-4h4v4zm0-12h-4v4h4v-4zm0-8h-4v4h4v-4zm3 0v4h17v-4h-17zm0 12h17v-4h-17v4zm0 8h17v-4h-17v4z"></path>
-          </svg>
-        </button> */}
-        <nav className="hidden md:block">
-          <ul>
-            <li>
-              <Link href="contact-page">
-                  <a className="text-black-300 hover:bg-blue-300 px-3 py-2 rouded">
-                    Contact
-                  </a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
       </header>
 
       {/* mainのタブでchidlenを表示する */}
