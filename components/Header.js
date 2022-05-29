f35cd6ec89a744987de2ba2fbf5750c70eb70674
base1551 <baseball.1.kyoto@gmail.com>
2022-05-29 10:46:26 +0900

    navbar修正

--- components/Header.js
+++ components/Header.js
@@ -1,37 +1,43 @@
 import Link from 'next/link'
+import siteMetadata from "../data/siteMetadata";
+import headerNavLinks from "../data/NavLinks";
+import ThemeSwich from "../components/ThemeSwich";
+import {useRouter} from "next/router";
+// import Logo from "../public/images/logo.svg";
+import MobileNav from "./MobileNav";
 
 export default function Header() {
+    const router = useRouter();
+
     return (
         <header className="relative flex items-center justify-between py-4 lg:px-20 px-4 border-b">
-            <div className="flex items-center">
-                <Link href="/">
-                    <a className="pr-10">おかBlog</a>
-                </Link>
+            <div>
+                <div className="flex items-center justify-between">
+                    <Link href="/" aria-label="OkaBlog">
+                        <a
+                            className="h-7 text-xl sm:text-xl font-semibold text-gray-900 sm:block">
+                            {siteMetadata.headerTitle}
+                        </a>
+                    </Link>
+                </div>
             </div>
-
-            <button className="md:hidden block">
-                <svg fill="currentColor" height="20" viewBox="0 0 24 24" width="20"
-                     xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M4 22h-4v-4h4v4zm0-12h-4v4h4v-4zm0-8h-4v4h4v-4zm3 0v4h17v-4h-17zm0 12h17v-4h-17v4zm0 8h17v-4h-17v4z"></path>
-                </svg>
-            </button>
-            <nav className="hidden md:block">
-                <ul className="flex items-center">
-                    <li className="ml-2">
-                        <Link href={"/"}>
-                        <a href="" rel="noopener noreferrer"
-                                            // target="_blank"
-                                            className="block text-blue-500 underline transition-all p-1 hover:no-underline">HOME</a></Link>
-                    </li>
-                    <li className="ml-2">
-                        <Link href={"/profile-page"}>
-                            <a href=""
-                               className="block text-blue-500 underline transition-all p-1 hover:no-underline">Profile</a></Link>
-                    </li>
-
-                </ul>
-            </nav>
+            <div className="flex items-center text-base leading-5">
+                <div className="hidden md:flex">
+                    {headerNavLinks.map((link) => (
+                        <div className="pl-5" key={link.title}>
+                            <Link
+                                href={link.href}
+                                className="font-medium text-gray-900 dark:text-gray-100 border-b-2 border-transparent hover:border-gray-900 dark:hover:border-gray-100 transition duration-300 ease-in-out m-2 py-2 md:mx-5"
+                            >
+                                {link.title}
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+                {/*<ThemeSwich/>*/}
+                <MobileNav/>
+            </div>
         </header>
+
     )
 }
---
+++ data/siteMetadata.js
@@ -0,0 +1,19 @@
+const siteMetadata = {
+    title: "OkaBlog",
+    author: "Oka",
+    belongs: "shiga",
+    headerTitle: "OkaBlog",
+    description:
+        "滋賀から発信するアウトプットブログ",
+    language: "ja",
+    siteUrl: "https://www.jock-blog.com/",
+    siteLogo: "/images/logo.png",
+    image: "/images/avatar.png",
+    socialBanner: "/images/twitter-card.png",
+    email: "",
+    github: "https://github.com/base1551",
+    twitter: "https://twitter.com/JockRock95",
+    locale: "jp",
+};
+
+module.exports = siteMetadata;
